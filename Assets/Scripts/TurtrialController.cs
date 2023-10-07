f85b26dadbe7ce2286de4b01d1bdb6e092903ac6
masatometoki <21c1020049mm@edu.tech.ac.jp>
2023-10-07 18:53:21 +0900

    Update TurtrialController.cs
    チュートリアル修正

--- Assets/Scripts/TurtrialController.cs
+++ Assets/Scripts/TurtrialController.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class TurtrialController : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas firstCanvas, secondCanvas, threeCanvas;
+
+    private int selectCanvas = 1; //選択中のイメージ
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowSelectedCanvas();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SelectCanvas();
+    }
+
+    private void SelectCanvas()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (selectCanvas == 1)
+            {
+                selectCanvas++;
+                ShowSelectedCanvas();
+                Debug.Log("プレイヤー選択中" + selectCanvas.ToString());
+            }
+            else if (selectCanvas < 2)
+            {
+                selectCanvas++;
+                ShowSelectedCanvas();
+                Debug.Log("プレイヤー選択中" + selectCanvas.ToString());
+            }
+            else if (selectCanvas < 3)
+            {
+                selectCanvas++;
+                ShowSelectedCanvas();
+                Debug.Log("プレイヤー選択中" + selectCanvas.ToString());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            if (selectCanvas > 1)
+            {
+                selectCanvas--;
+                ShowSelectedCanvas();
+                Debug.Log("プレイヤー選択中" + selectCanvas.ToString());
+            }
+            else if (selectCanvas > 2)
+            {
+                selectCanvas--;
+                ShowSelectedCanvas();
+                Debug.Log("プレイヤー選択中" + selectCanvas.ToString());
+            }
+            else if (selectCanvas > 3)
+            {
+                selectCanvas--;
+                ShowSelectedCanvas();
+                Debug.Log("プレイヤー選択中" + selectCanvas.ToString());
+            }
+        }
+    }
+
+    private void ShowSelectedCanvas()
+    {
+        if (selectCanvas == 1)
+        {
+            
+        }
+        else if (selectCanvas == 2)
+        {
+           
+        }
+        else if (selectCanvas == 3)
+        {
+
+        }
     }
 }
